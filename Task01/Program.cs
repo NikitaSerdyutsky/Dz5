d1acec7c0e33fd538c91be027cd414c249cb234e
Nikita <serduckijnikita97@gmail.com>
2023-09-25 22:58:25 +0300

    test

---
+++ Task01/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+
+
+void Evennumber(int[] arr)
+{
+    int sum = 0;
+    for (int l = 0; l < arr.Length; l++)
+    {
+        if (arr[l] % 2 == 0) sum++;
+
+    }
+    Console.WriteLine($"Чётных чисел {sum}");
+}
+
+Console.Write("Введите размер массива: ");
+int length = int.Parse(Console.ReadLine()!);
+int[] Array = CreateArrayRndInt(length, 1, 999);
+Console.WriteLine($"[{string.Join(", ", Array)}]");
+Evennumber(Array);
---
+++ Task02/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+
+
+void Evennumber(int[] arr)
+{
+    int odd = 0;
+    for (int j = 1; j < arr.Length; j = j + 2)
+    {
+        odd = odd + arr[j];
+    }
+    Console.WriteLine($"Сумма чисел на нечётных позициях {odd}");
+}
+
+Console.Write("Введите размер массива: ");
+int size = int.Parse(Console.ReadLine()!);
+Console.Write("Введите минимальное значение массива: ");
+int minValue = int.Parse(Console.ReadLine()!);
+Console.Write("Введите максимальное значение массива: ");
+int maxValue = int.Parse(Console.ReadLine()!);
+int[] Array = CreateArrayRndInt(size, minValue, maxValue);
+Console.WriteLine($"[{string.Join(", ", Array)}]");
+Evennumber(Array);
